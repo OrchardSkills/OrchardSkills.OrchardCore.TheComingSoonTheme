045f6e724d3102d9c2db7a3a6c0858b47464b150
Orchard Skills <Sales@OrchardSkills.com>
2020-01-14 07:06:33 -0700

    Added OrchardSkills.OrchardCore.TheComingSoonTheme

---
+++ OrchardSkills.OrchardCore.TheComingSoonTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Orchard Skills Coming Soon Theme",
+    Author = "Orchard Skills",
+    Website = "https://OrchardSkills.com",
+    Version = "2.0.0",
+    Description = "Orchard Skills Landing page for a project that is under construction"
+)]
